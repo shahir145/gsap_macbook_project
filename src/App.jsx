194453dfcb5c1856d929be12b0ce5660537f689e
Shahir <shahirali@outlook.com>
2025-10-16 21:55:29 +0100

    Implement 3D Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -4,22 +4,27 @@ import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14.jsx";
-import StudioLights from "./StudioLights.jsx";
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
+import {useMediaQuery} from "react-responsive";
 
 const ProductViewer = () => {
     const { color, scale, setColor, setScale } = useMacbookStore();
+
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
+
     return (
         <section id="product-viewer">
             <h2>Take a closer look.</h2>
 
             <div className="controls">
-                <p className="info">MacBook Pro {scale} in {color}</p>
+                <p className="info">MacBook Pro | Available in 13" & 16" in Space Grey and Dark Colours</p>
 
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
                         <div
-                            onClick={() => setColor('#adb5db')}
-                            className={clsx('bg-neutral-300', color === '#adb5db' && 'active')}
+                            onClick={() => setColor('#9EA1B0')}
+                            className={clsx('bg-neutral-300', color === '#9EA1B0' && 'active')}
                         />
                         <div
                             onClick={() => setColor('#2c2e2c')}
@@ -47,9 +52,7 @@ const ProductViewer = () => {
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
                 <StudioLights />
 
-                <MacbookModel14 scale={0.06} position={[0 , 0, 0]} />
-
-                <OrbitControls enableZoom={false} />
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile}/>
             </Canvas>
         </section>
     )
