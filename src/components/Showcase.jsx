91386708a228f0ac63a2464ec4000b84b7c78c3b
Shahir <shahirali@outlook.com>
2025-10-17 18:57:26 +0100

    Implement full Showcase section

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import {useMediaQuery} from "react-responsive";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Showcase = () => {
+    const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
+
+    useGSAP(() => {
+        if(!isTablet) {
+            const timeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '#showcase',
+                    start: 'top top',
+                    end: 'bottom top',
+                    scrub: true,
+                    pin: true,
+                    //snap: true,
+                }
+            })
+
+            timeline
+                .to('.mask img', {
+                    transform: 'scale(1.1)'
+                }).to('.content', { opacity: 1, y: 0, ease: 'power1.in' });
+        }
+    }, [isTablet])
+
     return (
         <section id="showcase">
             <div className="media">
-                <video src="/videos/game.mp4" loop muted playsInline />
+                <video src="/videos/game.mp4" loop muted autoPlay playsInline />
                 <div className="mask">
                     <img src="/mask-logo.svg" />
                 </div>
